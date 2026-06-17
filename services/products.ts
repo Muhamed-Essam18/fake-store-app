b21ea94f21bb4ac193d84605fe73e8864570a660
Muhamed Essam <muhamed.essam2@aiesec.net>
2026-06-17 16:00:20 +0300

    feat: create product type and api service

---
+++ services/products.ts
@@ -0,0 +1,13 @@
+import { Product } from "@/types/product";
+
+export const getProducts = async (): Promise<Product[]> => {
+  const response = await fetch(
+    "https://fakestoreapi.com/products"
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch products");
+  }
+
+  return response.json();
+};
